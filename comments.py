fe8ca8bf42b1662bc39b5d778f0966cbacadcb1a
Rakshith <rakshithhgd@gmail.com>
2021-08-22 11:24:56 +0530

    starting today

---
+++ comments.py
@@ -0,0 +1,17 @@
+#single line comment
+print("Have a good day")
+
+#There is no
+#syntax for multiline comments
+#in python
+print("Good evening")
+#or
+"""
+
+this is 
+a multiline 
+comment created by 
+using multiline string
+
+"""
+print("Wassup")
