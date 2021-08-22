878149e64ca45826f7b6da117c00c9bfa0cc9d6b
Rakshith <rakshithhgd@gmail.com>
2021-08-22 11:50:33 +0530

    starting today

---
+++ VariablesAssignMultiple.py
@@ -0,0 +1,17 @@
+#TO ASSIGN  VALUES TO MULTIPLE VARIABLES
+x, y, z = "orange", "mango", "pineapple"
+print(x)
+print(y)
+print(z)
+#TO ASSIGN SINGLE VALUE TO MULTIPLE VARIABLES
+X=Y=Z= "PUNISHER"
+print(X)
+print(Y)
+print(Z)
+
+#unpacking a collection 
+veggies = ["carrot","beans","tomato"]
+x,y,z = veggies
+print(x)
+print(y)
+print(z)
