952858d47e770fcd6e0cb05f7a8187099de2079f
Rakshith <rakshithhgd@gmail.com>
2021-08-22 11:36:19 +0530

    starting today

---
+++ variables.py
@@ -0,0 +1,28 @@
+x=5
+y = "RK"
+print (x)
+print(y)
+
+#variables do not need to be declared with any particular type 
+#it can change even after they have been set
+x = 6
+print(x)
+x= "Snapchat"
+print(x)
+
+#to specify the data type of a varianble
+#it can be done by typecasting
+x = str(4) #x will be "4"
+y = int(4) #y will be 4
+z = float(4) #z will be 4.0
+
+print (x,y,z)
+
+#to know the data type of a variable
+x = 5
+y = "Bottle"
+print(type(x))
+print(type(y))
+
+#strings can be declared using either single or double quotes
+#variables are case sensitive
