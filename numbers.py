557147551c95f0b7669a7664b007335f72741839
Rakshith <rakshithhgd@gmail.com>
2021-08-24 18:52:20 +0530

    24-08

---
+++ numbers.py
@@ -0,0 +1,15 @@
+#python has 3 types of built-in numbers
+#int float complex
+x = 1
+y= 2.9
+z = 8j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#python doesnot have a random() function but it has a built-in module called random to make random numbers
+import random
+
+print(random.randrange(1,10,2))
+
