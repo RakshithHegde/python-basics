a8146efc955f753a8d7e2f7b201175b0d94009df
Rakshith <rakshithhgd@gmail.com>
2021-08-24 21:52:29 +0530

    24-08

---
+++ boolean.py
@@ -0,0 +1,16 @@
+print(10<9)
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#bool() function allows you to evaluate any value and give you true or false in return
+
+print(bool("Hi"))
+print(bool(3))
+
+x = 200
+print(isinstance(x, int))
