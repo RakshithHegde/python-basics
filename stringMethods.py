da9b31e1893ab318fd13cc34bf03d2fbe63ec8da
Rakshith <rakshithhgd@gmail.com>
2021-08-24 19:37:04 +0530

    24-08

--- stringMethods.py
+++ stringMethods.py
@@ -28,3 +28,23 @@ print(a.replace("Evening","Night"))
 #split()
 a="What are you ,planning for today?"
 print(a.split(","))
+
+#concatenation of 2 strings
+a = "Hi"
+b= "Have a good day"
+c = a + "!," + b
+print(c)
+
+#format
+age = 20
+text = "My name is rk, and I am {}"
+print(text.format(age))
+#we can pass multiple values through format method as it can take unlimited number of arguments
+PetrolStation="Indian Oil"
+Petrolrate = 104.98
+PetrolAmount = 6000
+Myday = "Today I went to fill petrol at {} the petrol rate was {}//l and I spent {}rs "
+print(Myday.format(PetrolStation,Petrolrate,PetrolAmount))
+
+NextDay="I paid {2}rs today at {0} as the rate of petrol was {1}//l"
+print(NextDay.format(PetrolStation,Petrolrate,PetrolAmount))
